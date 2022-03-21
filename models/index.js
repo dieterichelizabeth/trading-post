e432845933922623f5b3c077fd42b352534cacb9
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-21 14:03:57 -0500

    adds logic to index.js models

--- models/index.js
+++ models/index.js
@@ -5,10 +5,21 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "",
+});
 
 // Tags belongToMany Products (through ProductTag)
 
