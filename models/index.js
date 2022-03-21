7272cf14f97becfb11a81ec7794f36547414fc9a
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-21 14:11:51 -0500

    adds logic to product.js models

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "",
+  foreignKey: "category_id",
   onDelete: "CASCADE",
 });
 
