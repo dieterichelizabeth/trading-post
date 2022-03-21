0fcea5e869f05abe26443954176a15f4be1c4e1d
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-21 14:17:09 -0500

    defines columns in ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,24 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Store a reference of the `id` of the `Product` which the tag belongs to
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
   {
     sequelize,
