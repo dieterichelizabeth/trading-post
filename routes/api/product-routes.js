47fe2d0bfc17c5843d02ae92d09c7b9ecedca1c5
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-21 23:54:14 -0500

    initial get routes for product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,17 +2,41 @@ const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
+// Cannot get assoicated Category data
 
 // get all products
 router.get("/", (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // find all products including its associated Category and Tag data
+  Product.findAll({
+    include: {
+      model: Category,
+      model: Tag,
+    },
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get("/:id", (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // find a single product by its `id` including its associated Category and Tag data
+  Product.findOne({
+    include: {
+      model: Category,
+      model: Tag,
+    },
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
